985475c92de93c5865f0d20196cc0a3257c1f269
skillshiftdigital <156049820+skillshiftdigital@users.noreply.github.com>
2024-06-11 20:54:29 +1000

    skillshift-next-Inception-11062024

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -13,12 +13,13 @@ export const client = createClient({
   apiVersion: '2024-05-27',
 }) */
 
-  import sanityClient from '@sanity/client';
+  import createClient from '@sanity/client';
 
-  const client = sanityClient({
+  const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn: true, // Set to false if you want fresh data
+    apiVersion: '2024-06-10', // use a UTC date string
     token: process.env.SANITY_API_READ_TOKEN, // Only if you need to use authenticated requests
   });
   
